75798fc3181672468ba27c75d026b08ce101fe43
Kelly M Curtis <Kelly@Kellys-MacBook-Pro.local>
2015-06-07 07:42:50 -0400

    added links to navbar for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   resources :info_pages
   resources :dogs
-  get 'welcome/index'
 
+  root 'welcome#index'
+
+  get 'main_page' => 'welcome#index'
   get 'about' => 'welcome#about'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
