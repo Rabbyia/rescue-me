dd147ad5d1c55476b80bd191d659524b4e152c61
Nicole Rudi <nicolejrudi@gmal.comssh-keygen rsa -C nicolejrudi@gmail.com>
2015-06-07 10:04:51 -0400

    adding just a few things

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605200525) do
+ActiveRecord::Schema.define(version: 20150607114924) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.string   "age"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "info_pages", force: :cascade do |t|
