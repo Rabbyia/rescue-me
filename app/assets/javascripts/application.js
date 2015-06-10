94f14079fd2d4fbe90537f551245019ea6957148
Kelly M Curtis <Kelly@Kellys-MacBook-Pro.local>
2015-06-10 09:40:33 -0400

    Updating Info page and update to site font to Papyrus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+jQuery(function(){
+
+    var minimized_elements = $('p.minimize');
+
+    minimized_elements.each(function(){
+        var t = $(this).text();
+        if(t.length < 50) return;
+
+        $(this).html(
+            t.slice(0,50)+'<span>... </span><a href="#" class="more">More</a>'+
+            '<span style="display:none;">'+ t.slice(50,t.length)+' <a href="#" class="less">Less</a></span>'
+        );
+
+    });
+
+    $('a.more', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).hide().prev().hide();
+        $(this).next().show();
+    });
+
+    $('a.less', minimized_elements).click(function(event){
+        event.preventDefault();
+        $(this).parent().hide().prev().show().prev().show();
+    });
+
+});
